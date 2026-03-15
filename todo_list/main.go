4cc3dd2243ad09a9913f141d6660b8f9f6fdbf0f
thallysson <thallysson@ciandt.com>
2026-03-15 11:31:38 -0300

    feat: todo list

---
+++ todo_list/main.go
@@ -0,0 +1,42 @@
+package main
+
+import "fmt"
+
+type Task struct {
+	Title string
+	Done bool
+}
+
+func main() {
+	var option int
+	var tasks []Task
+
+	for true {
+		fmt.Printf("Select 1 to Add and 2 to List and 3 to exit and 4 to check items: ")
+		fmt.Scan(&option)
+
+		if option == 3 {
+			break;
+		} else if option == 1 {
+			fmt.Printf("Type the task title: ")
+			var title string
+			fmt.Scan(&title)
+			tasks = append(tasks, Task{
+				Title: title,
+				Done: false,
+			})
+		} else if option == 2 {
+			for i, task := range tasks {
+				fmt.Printf("Id: %d, Title: %s, Done: %t\n", i, task.Title, task.Done)
+			}
+		} else if option == 4 {
+			fmt.Printf("Type the task id: ")
+			var taskId int
+			fmt.Scan(&taskId)
+			fmt.Printf("Type 1 for check and 2 to uncheck: ")
+			var check int
+			fmt.Scan(&check)
+			if check == 1 { tasks[taskId].Done = true } else { tasks[taskId].Done = false }
+		}
+	}
+}
